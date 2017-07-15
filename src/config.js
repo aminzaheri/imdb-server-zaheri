976782e2f58dfe67d25a98373cd56d5bd819e39f
mehdad1420 <mehdad.qasemian@gmail.com>
2017-07-15 01:46:12 -0700

    First commit

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 let config = {};
 
-config.authInstanceId = "<<X-Backtory-Authentication-Id>>";
-config.objectSorageInstanceId = "<<X-Backtory-Object-Storage-Id>>";
-config.integratedMasterKey = "<<X-Backtory-Authentication-Key (Master)>>";
-config.clientKey = "<<X-Backtory-Authentication-Key (Client)>>";
-config.cloudCode = "<<Cloud-Code-Id>>";
-config.cdnInstanceId = "<<X-Backtory-Storage-Id>>";
+config.authInstanceId = "5969d3c3e4b0d93035ae638b";
+config.objectSorageInstanceId = "5969d3c4e4b0a69c4974be3a";
+config.integratedMasterKey = "140f8891a1224de19fcc9945";
+config.clientKey = "5969d3c3e4b0fc0b3ebbc009";
+config.cloudCode = "5969d3c4e4b0a69c4974be42";
+config.cdnInstanceId = "5969d3c4e4b0a69c4974be3a";
 
-config.baseUrl = "http://storage.backtory.com/<<cdn address>>";
+config.baseUrl = "http://storage.backtory.com/imdbZaheri";
 
 
 config.lambdaHeaders =
